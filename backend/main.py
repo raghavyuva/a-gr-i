13ae92d26d3c250a1c0e493b7a45f8040ff38a30
Abhishek Bhat <abhishekbhat.dev@gmail.com>
2023-06-10 20:53:58 +0530

    backend routers set

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,27 @@
 # main.py
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile,File
 from pydantic import BaseModel
 from PIL import Image
 import io
 # import tensorflow as tf
-import numpy as np
+from router import user
+
+import numpy as np 
 
 # Define the FastAPI app
 app = FastAPI()
+
+app.include_router(user.router)
 
 # Define the input data schema using Pydantic
 class ImageRequest(BaseModel):
     image: UploadFile
 
+# Define the input data schema for cause
+class CauseInput(BaseModel):
+    data : str
 # Load the trained TensorFlow model
-model = tf.keras.models.load_model('path_to_your_model.h5')
+# model = tf.keras.models.load_model('path_to_your_model.h5')
 
 # Define the prediction route
 @app.post("/predict")
@@ -27,11 +34,24 @@ def predict(image: UploadFile = File(...)):
     image_array = np.expand_dims(image_array, axis=0)  # Add a batch dimension
 
     # Perform prediction using the loaded model
-    prediction = model.predict(image_array)
+    # prediction = model.predict(image_array)
 
     # Convert the prediction to a human-readable label
-    labels = ['class1', 'class2', 'class3']  # Replace with your actual class labels
-    predicted_label = labels[np.argmax(prediction)]
+    # labels = ['class1', 'class2', 'class3']  # Replace with your actual class labels
+    # predicted_label = labels[np.argmax(prediction)]
 
     # Return the prediction as the API response
-    return {"prediction": predicted_label}
+    # return {"prediction": predicted_label}
+
+
+@app.post('/prompt/cause')
+async def getCause( cause : CauseInput ):
+    return {
+        "message" : "Calling Cause route"
+    }
+
+@app.get('/')
+def server():
+    return {
+        "message" : "Server is working"
+    }
